1c02f8f402e9fed77bb919d6d4b939246d2810bd
Jarneke <Jarne.Eldrup@outlook.com>
2024-01-05 01:49:01 +0100

    updated javascript to add buffer to slide in effect, changed some stuff in html and css but htose are minor

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+  { rootMargin: "-218px 0px" }
+);
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
