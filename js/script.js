fb5d7fdf3a5b2987275f9d577726f56dd258b7ae
Jarneke <Jarne.Eldrup@outlook.com>
2024-01-08 16:23:46 +0100

    whole new redesign

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+const vhInPixels = window.innerHeight || document.documentElement.clientHeight;
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
+      console.log(entry);
+
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
@@ -8,8 +11,7 @@ const observer = new IntersectionObserver(
       }
     });
   },
-  { rootMargin: "-218px 0px" }
+  { rootMargin: `-${vhInPixels / 3}px 0px` }
 );
-
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
